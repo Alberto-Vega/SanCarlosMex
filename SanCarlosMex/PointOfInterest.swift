4b2110e1950d1b8c78bde184d54f5bec6553d293
Alberto-Vega <el_vega@live.com>
2015-10-15 22:11:30 -0700

    Added table view to choose activities

---
+++ SanCarlosMex/PointOfInterest.swift
@@ -0,0 +1,9 @@
+//
+//  PointOf.swift
+//  SanCarlosMex
+//
+//  Created by Alberto Vega Gonzalez on 10/15/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
