ccb94849c4f199f10bee08e09bec280f211783a0
Alberto-Vega <el_vega@live.com>
2015-10-17 22:39:15 -0700

    Added Category class & functionality to display categories.names on tableView

---
+++ SanCarlosMex/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  SanCarlosMex
+//
+//  Created by Alberto Vega Gonzalez on 10/17/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
