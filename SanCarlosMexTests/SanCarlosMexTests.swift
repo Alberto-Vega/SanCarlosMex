ff57e014d801da3e067b1f0aa9843c20ced6e0e3
Alberto-Vega <el_vega@live.com>
2016-01-19 00:10:33 -0800

    Added Unit testing for class initializers: PointOfInterest and Restaurants.

--- SanCarlosMexTests/SanCarlosMexTests.swift
+++ SanCarlosMexTests/SanCarlosMexTests.swift
@@ -2,12 +2,14 @@
 //  SanCarlosMexTests.swift
 //  SanCarlosMexTests
 //
-//  Created by Alberto Vega Gonzalez on 6/12/15.
-//  Copyright (c) 2015 Alberto Vega Gonzalez. All rights reserved.
+//  Created by Alberto Vega Gonzalez on 1/18/16.
+//  Copyright © 2016 Alberto Vega Gonzalez. All rights reserved.
 //
 
-import UIKit
+import MapKit
 import XCTest
+
+@testable import SanCarlosMex
 
 class SanCarlosMexTests: XCTestCase {
     
@@ -21,14 +23,23 @@ class SanCarlosMexTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    
+    func testPointOfInterstInitialization() {
+        // Success case.
+        
+        let potentialPointOfInterest = PointOfInterest(name: "No Coordinate POI", shortDescription: "No Coordinate POI", address: "Cupertino Road 123, Seattle, WA", features: "No Coordinate POI", hours: "No Coordinate POI", notes: "No Coordinate POI", image: "NoCoordinatePOI.jpg", coordinate: CLLocationCoordinate2D(latitude: 41.40338, longitude: 2.17403))
+        XCTAssertNotNil(potentialPointOfInterest)
+    }
+    
+    func testRestaurantInitialization() {
+        // Success case.
+       let potentialRestaurant = Restaurant(name: "Piccolo", shortDescription: "Italian Food", address: "Beltrones Avenue, San Carlos, Mexico", features: "Not only is the food really good, but the atmosphere is cozy and inviting.", hours: "Wed - Sat 6:30 pm - 11:00 pm", notes: "Great food and good service.", image: "piccolo.jpg",coordinate: CLLocationCoordinate2D(latitude: 27.9603909, longitude: -111.0347736), cuisine: "Italian", phone: "622-226-0503")
+        XCTAssertNotNil(potentialRestaurant)
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
